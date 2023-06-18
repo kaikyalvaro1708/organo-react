63f9747b3eecd914301d517c6fac530886be7cad
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-06-18 16:41:42 -0300

    primeiro commit

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,10 @@
+import imgBanner from '../../imagens/banner.jpg';
+import './estilo.css'
+
+function Banner (){
+    return(
+        <img src={imgBanner} alt="Banner principal da página Organo"></img>
+    )
+}
+
+export default Banner
